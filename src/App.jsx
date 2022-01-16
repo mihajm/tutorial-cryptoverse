d5c72d0e3735e74bfe5cf8a1a9af38f2d7c76048
miham <miham@marand.si>
2022-01-16 16:08:13 +0100

    added navbar base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
+
+import {Navbar} from './components';
 
 const App = () => (
-	<div>
+	<div className="app">
+		<div className="navbar">
+			<Navbar />
+		</div>
+		<div className="main">
 
+		</div>
+		<div className="footer">
+
+		</div>
 	</div>
 );
 
