6e60ffb3da00fa65c66fab4cb5b3450ca9a30f01
miham <miham@marand.si>
2022-01-16 16:18:52 +0100

    added menu & refactor names

---
+++ src/components/navbar/AppLogo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Typography, Avatar} from 'antd';
+import {Link} from 'react-router-dom';
+
+import icon from '../../images/cryptocurrency.png';
+
+const AppLogo = () => (
+	<div className="logo-container">
+		<Avatar src={icon} size="large" />
+		<Typography.Title level={2} className="logo">
+			<Link to="/">Cryptoverse</Link>
+		</Typography.Title>
+		{/* <Button className="menu-control-container"></Button> */}
+	</div>
+);
+
+export default AppLogo;
---
+++ src/components/navbar/AppNavbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AppLogo from './AppLogo';
+import AppMenu from './AppMenu';
+
+const AppNavbar = () => (
+	<div className="nav-container">
+		<AppLogo />
+		<AppMenu />
+	</div>
+);
+
+export default AppNavbar;
